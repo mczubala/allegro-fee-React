6f1541b3df7b413707c13a37eb373123bd432681
marcinczubala <czubala.marcin@gmail.com>
2023-12-21 23:34:12 +0100

    adjusted to new controller

--- src/App.js
+++ src/App.js
@@ -1,72 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 import OfferFeePreview from './OfferFeePreview';
-
 function App() {
-  const [productId, setProductId] = useState('');
-  const [product, setProduct] = useState(null);
   const [offerId, setOfferId] = useState('');
   const [offerFee, setOfferFee] = useState(null);
+  
 
-  const fetchProduct = async () => {
+  const fetchCalculatedTotalOfferFee = async () => {
     try {
-      const response = await fetch(`http://localhost:5029/Product/${productId}`);
+      const response = await fetch(`http://localhost:5029/fee/get-total-offer-fee/${offerId}`);
       if (response.ok) {
-        const productData = await response.json();
-        setProduct(productData);
+        const offerFeeData = await response.json();
+        console.log("Offer Fee Data:", offerFeeData);
+        setOfferFee(offerFeeData);
       } else {
-        setProduct(null);
-        alert('Product not found');
+        setOfferFee(null);
+        alert('Offer fee not found');
       }
     } catch (error) {
-      alert('Error fetching product');
+      alert('Error fetching offer fee');
     }
   };
 
-  const fetchOfferFeePreview = async () => {
-    try {
-      const response = await fetch(`http://localhost:5029/Product/offer-fee-preview/${offerId}`);
-      if (response.ok) {
-      const offerFeeData = await response.json();
-        console.log("Offer Fee Data:", offerFeeData); // Add this line
-        const parsedOfferFeeData = JSON.parse(JSON.stringify(offerFeeData));
-      setOfferFee(parsedOfferFeeData);
-    } else {
-      setOfferFee(null);
-      alert('Offer fee preview not found');
-    }
-  } catch (error) {
-    alert('Error fetching offer fee preview');
-  }
-};
-
   return (
-    <div className="App">
-      <h1>Product Finder</h1>
-      <input
-        type="text"
-        placeholder="Enter product ID"
-        value={productId}
-        onChange={(e) => setProductId(e.target.value)}
-      />
-      <button onClick={fetchProduct}>Find Product</button>
-      {product && (
-        <div>
-          <h2>Product Details</h2>
-          {/* Display product details */}
-        </div>
-      )}
-
-      <h1>Offer Fee Preview Finder</h1>
-      <input
-        type="text"
-        placeholder="Enter offer ID"
-        value={offerId}
-        onChange={(e) => setOfferId(e.target.value)}
-      />
-      <button onClick={fetchOfferFeePreview}>Find Offer Fee Preview</button>
-      {offerFee && <OfferFeePreview offerFee={offerFee} />}
-    </div>
+      <div className="App">
+  
+        <h1>Offer Fee Calculator</h1>
+        <input
+            type="text"
+            placeholder="Enter offer ID"
+            value={offerId}
+            onChange={(e) => setOfferId(e.target.value)}
+        />
+        <button onClick={fetchCalculatedTotalOfferFee}>Calculate Offer Fee</button>
+        {offerFee && <OfferFeePreview offerFee={offerFee} />}
+      </div>
   );
 }
 
--- src/OfferFeePreview.js
+++ src/OfferFeePreview.js
@@ -5,47 +5,18 @@ const OfferFeePreview = ({ offerFee }) => {
         return null;
     }
 
-    const formatCurrency = (amount, currency) => {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency,
-        }).format(amount);
+    const formatCurrency = (amount) => {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'PLN' }).format(amount);
     };
-
-    const combinedFees = offerFee.commissions.concat(offerFee.quotes).filter(fee => fee.fee && fee.fee.amount);
-    const totalAmount = combinedFees.reduce(
-        (total, fee) => total + parseFloat(fee.fee.amount),
-        0
-    );
-
-    const defaultCurrency = 'PLN';
-    const currency = offerFee.commissions.length > 0 && offerFee.commissions[0].fee && offerFee.commissions[0].fee.currency
-        ? offerFee.commissions[0].fee.currency
-        : defaultCurrency;
 
     return (
         <div>
             <h2>Offer Fee Preview</h2>
-            <table>
-                <thead>
-                <tr>
-                    <th>Fee Name</th>
-                    <th>Fee Type</th>
-                    <th>Amount</th>
-                </tr>
-                </thead>
-                <tbody>
-                {combinedFees.map((fee, index) => (
-                    <tr key={index}>
-                        <td>{fee.name}</td>
-                        <td>{fee.type}</td>
-                        <td>{formatCurrency(fee.fee.amount, fee.fee.currency)}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
             <p>
-                <strong>Total: </strong> {formatCurrency(totalAmount, currency)}
+                <strong>Offer ID:</strong> {offerFee.offerId}
+            </p>
+            <p>
+                <strong>Fee:</strong> {formatCurrency(offerFee.fee)}
             </p>
         </div>
     );
